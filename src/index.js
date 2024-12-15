3065afa9583597091f563f5f7e40bf6d66424788
Aosegir <128326345+Aosegir@users.noreply.github.com>
2024-12-15 14:53:17 -0800

    created event listener that keeps track of scroll up and down for screen transitions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "./style.css";
+let main = document.querySelector('main');
 
 console.log("Hello Odin");
+console.log(main);
+
+main.addEventListener('wheel', (event) => {
+    if(event.deltaY < 0) {
+        console.log("scroll up");
+    } else if(event.deltaY > 0) {
+        console.log("scroll down");
+    };
+});
